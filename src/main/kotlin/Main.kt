c94c0295de0f7f5727052f8901ea4a025c7e598f
Ali-Mwanasha <alimwanashaomari@gmail.com>
2024-04-02 22:34:10 +0300

    addedfile

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+    var x=20
+    var y=10
+    var sum:Int
+    sum=isAddition(x,y)
+   println("$sum")
+    var firstName="Anna"
+        var lastName="Onyango"
+
+}
+fun isAddition(x:Int,y:Int):Int{
+   var sum=x+y
+    var sumInt=sum.toInt()
+    return sumInt
+}
